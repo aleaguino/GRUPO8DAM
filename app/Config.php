0c90c94e7361b3fa48e82ac7cda61e3cb5ea4e91
4drielr <152649987+4drielr@users.noreply.github.com>
2024-12-03 13:11:28 +0100

    Integrante 3

---
+++ app/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config
+{
+    static public $mvc_bd_hostname = "localhost";
+    static public $mvc_bd_nombre   = "alimentos82024";
+    static public $mvc_bd_usuario  = "mygrupo8dad6";
+    static public $mvc_bd_clave    = "Q84tRml0";
+    static public $mvc_vis_css     = "estilo.css";
+}
